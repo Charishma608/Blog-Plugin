64011429d7554a2df867d2a85bb03f94f73e8d97
Charishma Bollineni <139740665+Charishma608@users.noreply.github.com>
2024-12-30 19:21:35 +0530

    Update Dashboard.js

--- blog-frontend/src/components/user/Dashboard.js
+++ blog-frontend/src/components/user/Dashboard.js
@@ -1,6 +1,5 @@
-
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom'; 
 import SideDashboard from './SideDashboard';
 import HomePage from './HomePage';
 import CreatePost from './CreatePost';
@@ -11,8 +10,11 @@ const Dashboard = () => {
       <SideDashboard />
       <div className="dashboard-content">
         <Routes>
+          <Route index element={<HomePage />} /> 
           <Route path="home" element={<HomePage />} />
           <Route path="create-post" element={<CreatePost />} />
+          
+          <Route path="*" element={<Navigate to="home" replace />} />
         </Routes>
       </div>
     </div>
